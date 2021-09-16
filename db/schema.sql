0a0ac66e2a50d32d58bd36ce8b87107e1e9376a3
Jennifer Ayala <j.ayala0695@gmail.com>
2021-09-15 18:24:19 -0700

    added tables  with column names

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS
+employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE department (
+id INT NOT NULL PRIMARY KEY,
+dep_name VARCHAR(30) NOT NULL,
+);
+
+DROP TABLE IF EXISTS role;
+
+CREATE TABLE role (
+id INT NOT NULL PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL
+);
+
+
+
