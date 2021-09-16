b738c5ff2a60607e49e7cda54fb1fb4a1ef494d5
Jennifer Ayala <j.ayala0695@gmail.com>
2021-09-15 18:40:40 -0700

    added last table

--- db/schema.sql
+++ db/schema.sql
@@ -12,14 +12,25 @@ id INT NOT NULL PRIMARY KEY,
 dep_name VARCHAR(30) NOT NULL,
 );
 
-DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee_role;
 
-CREATE TABLE role (
+CREATE TABLE employee_role (
 id INT NOT NULL PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL NOT NULL,
 department_id INT NOT NULL
 );
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee (
+id INT NOT NULL PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT NOT NULL
+);
+
+
 
 
 
