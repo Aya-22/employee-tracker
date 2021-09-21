0f5b34f73859361edd64a8313602c2cfbf4f91cf
Jennifer Ayala <j.ayala0695@gmail.com>
2021-09-20 17:50:44 -0700

    added tables to schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -18,7 +18,9 @@ CREATE TABLE employee_role (
 id INT NOT NULL PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL NOT NULL,
-department_id INT NOT NULL
+department_id INT NOT NULL,
+FOREIGN KEY (department_id)
+REFERENCES department(id)
 );
 DROP TABLE IF EXISTS employee;
 
@@ -27,7 +29,9 @@ id INT NOT NULL PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
-manager_id INT NOT NULL
+manager_id INT NOT NULL,
+FOREIGN KEY (role_id)
+REFERENCES employee_role(id)
 );
 
 
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO department (id, dep_name)
+VALUES (001, "Engineering"),
+(002, "Finance"), (003, "Legal"), (004, "Sales");
+
+INSERT INTO employee_role (id, title, salary, department_id)
+VALUES (1, "Sales Lead", 10000), (2, "Salesperson", 80000)
