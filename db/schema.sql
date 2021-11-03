7aa2743a6c088e9959200aa2a05f6335c2c6e4d5
Jennifer Ayala <j.ayala0695@gmail.com>
2021-11-02 19:19:57 -0700

    updated functions on index.js, added view functions, updated seeds

--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,7 @@ DROP TABLE IF EXISTS employee_role;
 CREATE TABLE employee_role (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
-salary DECIMAL NOT NULL,
+salary DECIMAL (10,0) NOT NULL,
 department_id INT NOT NULL,
 FOREIGN KEY (department_id)
 REFERENCES department(id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,10 @@
+
 INSERT INTO department (dep_name)
 VALUES ("Engineering"),
 ("Finance"), ("Legal"), ("Sales");
 
 INSERT INTO employee_role (title, salary, department_id)
-VALUES ("Sales Lead", 10000, 4), ("Salesperson", 80000, 4)
+VALUES ("Sales Lead", 100000, 4), ("Salesperson", 80000, 4), ("Salesperson", 80000, 4), ("Sales Manager", 150000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES (John, Doe, 1, 1)
+VALUES ("John", "Doe", 1, 1), ("Dave", "Wade", 2, 1), ("Joe", "Roland", 3, 1), ("Stranger", "Danger", 4, 1)
